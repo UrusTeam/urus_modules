31e12f8c8ef820cd3c0a3ae60b9c569e56ce5538
hiro2233 <htakey@gmail.com>
2017-07-26 03:51:28 -0400

    AP_HAL_URUS: dummy_libs: Updated GSC_Mavlink dummy library.

--- libraries/AP_HAL_URUS/dummy_libs/GCS_Mavlink.cpp
+++ libraries/AP_HAL_URUS/dummy_libs/GCS_Mavlink.cpp
@@ -1,11 +1,5 @@
 #include "GCS_Mavlink.h"
 
-void GCS_MAVLINK::send_parameter_value_all(char const*, ap_var_type, float)
-{
-    return;
-}
-
-void GCS_MAVLINK::packetReceived(__mavlink_status const&, __mavlink_message&)
-{
-    
-}
+const AP_Param::GroupInfo GCS_MAVLINK::var_info[] = {
+    AP_GROUPEND
+};
