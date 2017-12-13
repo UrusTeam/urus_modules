9afb3b281857ac4981dc01c1b0837a665a4aca31
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2017-12-13 16:00:37 -0400

    AP_HAL_URUS: Added system dummy libs.

---
+++ libraries/AP_HAL_URUS/dummy_libs/system_dummy.cpp
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/system.h>
+
+#if CONFIG_HAL_BOARD == HAL_BOARD_URUS
+
+#include "../CORE_URUS/CORE_URUS.h"
+
+#include <stdio.h>
+
+const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+
+#endif
