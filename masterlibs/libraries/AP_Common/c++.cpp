f164b500caf02b157a2adeae5ac35c8c238bef1f
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2017-12-19 03:15:47 -0400

    masterlibs: AP_Common: updated cxa macro for APM1 and APM2.

--- masterlibs/libraries/AP_Common/c++.cpp
+++ masterlibs/libraries/AP_Common/c++.cpp
@@ -36,3 +36,26 @@ void operator delete[](void * ptr)
 {
     if (ptr) free(ptr);
 }
+
+#if (CONFIG_SHAL_CORE == SHAL_CORE_APM)
+/* Needed for the AVR compiler */
+extern "C" void __cxa_pure_virtual()
+{
+    while (1){}
+}
+
+__extension__ typedef int __guard __attribute__((mode (__DI__)));
+
+int __cxa_guard_acquire(__guard *g)
+{
+    return !*(char *)(g);
+};
+
+void __cxa_guard_release (__guard *g){
+    *(char *)g = 1;
+};
+
+void __cxa_guard_abort (__guard *) {
+};
+
+#endif
