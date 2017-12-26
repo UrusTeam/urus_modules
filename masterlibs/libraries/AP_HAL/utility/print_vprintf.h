44f1bfd4dd5089c2032f313e921305dd69cd51dc
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2017-12-26 16:39:16 -0400

    masterlibs: AP_HAL: Updated Utility for PSTR and Progmem.

--- masterlibs/libraries/AP_HAL/utility/print_vprintf.h
+++ masterlibs/libraries/AP_HAL/utility/print_vprintf.h
@@ -4,4 +4,8 @@
 
 #include <AP_HAL/AP_HAL.h>
 
+#if CONFIG_SHAL_CORE == SHAL_CORE_APM
+void print_vprintf (AP_HAL::Print *s, unsigned char in_progmem, const char *fmt, va_list ap);
+#else
 void print_vprintf(AP_HAL::Print *s, const char *fmt, va_list ap);
+#endif
