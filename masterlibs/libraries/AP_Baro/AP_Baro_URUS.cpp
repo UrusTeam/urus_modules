8cf586c4b39350d7c31f463342ce743a68fab1d5
Hiroshi Takey (hiro2233) <htakey@gmail.com>
2019-05-04 21:38:51 -0400

    AP_Baro: Removed hil mode.

--- masterlibs/libraries/AP_Baro/AP_Baro_URUS.cpp
+++ masterlibs/libraries/AP_Baro/AP_Baro_URUS.cpp
@@ -7,17 +7,11 @@ AP_Baro_URUS::AP_Baro_URUS(AP_Baro &baro) :
     AP_Baro_Backend(baro)
 {
     _instance = _frontend.register_sensor();
-    _frontend.setHIL(0,1013,30,300,2,AP_HAL::millis());
 }
 
 // Read the sensor
 void AP_Baro_URUS::update(void)
 {
-    _frontend.set_hil_mode();
-    if (_frontend._hil.updated) {
-        _frontend._hil.updated = false;
-        _copy_to_frontend(0, _frontend._hil.pressure, _frontend._hil.temperature);
-        _frontend.setHIL(0,1013,30,300,2,AP_HAL::millis());
-    }
+    _copy_to_frontend(_instance, 96646, 23);
 }
 #endif // CONFIG_HAL_BOARD == HAL_BOARD_URUS
